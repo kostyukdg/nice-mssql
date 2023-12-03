0146732c2a22ac35351920d2d74f3b23dbbc0cf9
kostyukdg <kostyukdg@gmail.com>
2023-12-03 22:18:44 +0300

    Fix Request query method types

--- src/Request.ts
+++ src/Request.ts
@@ -2,30 +2,41 @@ import {
   Request as RequestOriginal,
   MSSQLError as MSSQLErrorOriginal,
   Table,
+  Promise,
+  IResult,
 } from 'mssql';
 import { MssqlError } from './MssqlError';
 
-export class Request extends RequestOriginal {
-  static async errorWrapper<T>(callback: () => Promise<T>) {
-    try {
-      return await callback();
-    } catch (_error: unknown) {
-      let error = _error;
-      if (_error instanceof MSSQLErrorOriginal) {
-        error = new MssqlError(_error.message);
-      }
-      if (error instanceof Error)
-        Error.captureStackTrace(error, Request.errorWrapper);
-      throw error;
+async function wrapError<T>(callback: () => Promise<T>) {
+  try {
+    return await callback();
+  } catch (_error: unknown) {
+    let error = _error;
+    if (_error instanceof MSSQLErrorOriginal) {
+      error = new MssqlError(_error.message);
     }
+    if (error instanceof Error) Error.captureStackTrace(error, wrapError);
+    throw error;
+  }
+}
+
+export class Request extends RequestOriginal {
+  public query<Entity>(command: string): Promise<IResult<Entity>>;
+  public query<Entity>(
+    command: TemplateStringsArray,
+    ...interpolations: never[]
+  ): Promise<IResult<Entity>>;
+  public query(
+    command: TemplateStringsArray | string,
+    ...interpolations: never[]
+  ) {
+    if (typeof command === 'string') {
+      return wrapError(() => super.query(command));
+    }
+    return wrapError(() => super.query(command, ...interpolations));
   }
 
-  // @ts-expect-error Multiple overrides problems
-  public async query(command: string) {
-    return Request.errorWrapper(async () => super.query(command));
-  }
-
-  public async bulk(table: Table) {
-    return Request.errorWrapper(async () => super.bulk(table));
+  public bulk(table: Table) {
+    return wrapError(() => super.bulk(table));
   }
 }
